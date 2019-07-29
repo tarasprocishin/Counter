193049e528eea7d5a78c0b16da8ab6ea279d0f2d
tarasprocishin <taras1procishin@gmail.com>
2019-07-29 21:54:05 +0300

    implement base logick/

---
+++ src/Counter.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+
+import './Counter.css';
+
+class Counter extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      number: 0,
+      toglle: true
+    }
+  }
+
+  increment = () => {
+    this.setState(prevState => {
+      let number = prevState.number + 1
+
+      if (number >= 10){
+        alert('your put maximum');
+        number = 10;
+      }
+      return { number: number }
+    })
+  }  
+
+  decrement = () => {
+    this.setState(prevState => ({
+        number: prevState.number - 1
+      }))
+  } 
+
+  toglleVive = () => {
+    this.setState(prevState => ({
+        toglle: !prevState.toglle
+      }))
+  } 
+
+  render() {
+    return (
+      <div className="Counter">
+        <p>
+          {this.state.toglle ? this.state.number: null}
+        </p>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
+        <button onClick={this.toglleVive}>{this.state.toglle?"Hide": "Show"}</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
